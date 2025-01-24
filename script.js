307c62924b3a5a6dc4299bbf4e5ff72ef57b429b
Bladimir Benavides <60237373+Blado87@users.noreply.github.com>
2025-01-24 15:00:42 -0500

    card

---
+++ script.js
@@ -0,0 +1,31 @@
+const metas = [
+  {
+    area: "Competencias",
+    lider: "Juan Pérez",
+    foto: "./img/juan.jpg",
+    color: "",
+    meta: 1000,
+    avanceTotal: 600,
+    avanceMensual: [100, 120, 80, 150, 90, 60]
+  },
+  {
+    area: "Empleo",
+    lider: "María Gómez",
+    foto: "./img/maria.jpg",
+    color: "",
+    meta: 800,
+    avanceTotal: 500,
+    avanceMensual: [80, 100, 120, 70, 60, 70]
+  },
+  {
+    area: "Población",
+    lider: "Carlos Rodríguez",
+    foto: "./img/carlos.jpg",
+    color: "",
+    meta: 1500,
+    avanceTotal: 900,
+    avanceMensual: [200, 180, 170, 150, 100, 100]
+  }
+];
+
+
